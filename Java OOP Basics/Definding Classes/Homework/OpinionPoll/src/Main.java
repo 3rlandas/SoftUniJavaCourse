c964f217dbc5bf6cf3d5ca4ba89ef5e189e80783
Erlandas <3rlandas@gmail.com>
2023-02-28 11:29:12 +0000

    done

--- Java OOP Basics/Definding Classes/Homework/OpinionPoll/src/Main.java
+++ Java OOP Basics/Definding Classes/Homework/OpinionPoll/src/Main.java
@@ -32,3 +32,57 @@ public class Main {
         }
     }
 }
+
+
+/*import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+          Set<Person> people = new TreeSet<>();
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+
+            String[] tokens = scanner.nextLine().split("\\s+");
+
+            String name = tokens[0];
+            int age = Integer.parseInt(tokens[1]);
+
+            Person person = new Person(name, age);
+
+            if (person.getAge() > 30) {
+                people.add(person);
+            }
+
+        }
+
+        for (Person person : people) {
+            System.out.println(person);
+        }
+    }
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
