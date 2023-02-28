7b42e59a5a6f869c72f510db0c3ad28c4cc2261c
Erlandas <3rlandas@gmail.com>
2023-02-28 11:40:50 +0000

    done

---
+++ Java OOP Basics/Definding Classes/Homework/CompanyRoster/src/Employee.java
@@ -0,0 +1,14 @@
+public class Employee {
+
+    private String name;
+    private double salary;
+    private String position;
+    private String department;
+    private String email = "n/a";
+    private int age = -1;
+
+    String Employee(){
+
+    }
+
+}
