7ae303ea84329b6c8afe7559274a57e4cc23a39b
Erlandas <3rlandas@gmail.com>
2023-03-01 11:53:30 +0000

    done

--- Java OOP Basics/Definding Classes/Homework/CompanyRoster/src/Employee.java
+++ Java OOP Basics/Definding Classes/Homework/CompanyRoster/src/Employee.java
@@ -4,11 +4,65 @@ public class Employee {
     private double salary;
     private String position;
     private String department;
-    private String email = "n/a";
-    private int age = -1;
+    private String email;
+    private int age;
 
-    String Employee(){
+    public Employee(String name, double salary, String position, String department) {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
 
+        this.email = "n/a";
+        this.age = -1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
     }
 
 }
--- Java OOP Basics/Definding Classes/Homework/CompanyRoster/src/Main.java
+++ Java OOP Basics/Definding Classes/Homework/CompanyRoster/src/Main.java
@@ -1,8 +1,54 @@
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        Map<String, List<Employee>> departmentSalaries = new HashMap<>();
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+
+            String[] input = scanner.nextLine().split("\\s+");
+
+            String name = input[0];
+            double salary = Double.parseDouble(input[1]);
+            String position = input[2];
+            String department = input[3];
+
+            Employee employee = new Employee(name, salary, position, department);
+
+            if (input.length == 5) {
+
+                Pattern pattern = Pattern.compile("^\\d+$");
+                Matcher matcher = pattern.matcher(input[4]);
+                if (matcher.find()) {
+                    employee.setAge(Integer.parseInt(input[4]));
+                } else {
+                    employee.setEmail(input[4]);
+                }
+            } else if (input.length == 6) {
+
+                employee.setEmail(input[4]);
+                employee.setAge(Integer.parseInt(input[5]));
+            }
+
+            List<Employee> employees;
+            if (!departmentSalaries.containsKey(department)) {
+                employees = new ArrayList<>();
+            } else {
+                employees = departmentSalaries.get(department);
+            }
+            employees.add(employee);
+            departmentSalaries.put(department, employees);
+        }
+
+
+
+
 
 
     }
