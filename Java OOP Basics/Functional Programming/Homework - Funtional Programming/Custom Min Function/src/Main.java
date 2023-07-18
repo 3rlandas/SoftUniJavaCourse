b6b132f292bcea355b0f53572e7ec93631f175d5
Erlandas <3rlandas@gmail.com>
2023-07-18 20:28:06 +0100

    almost done

---
+++ Java OOP Basics/Functional Programming/Homework - Funtional Programming/Custom Min Function/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::valueOf).collect(Collectors.toList());
+
+        Function<List<Integer>, Integer> findSmallestNumber = number -> {
+            int min = Integer.MAX_VALUE;
+            for (Integer integer : number) {
+                if (integer < min) {
+                    min = integer;
+                }
+            }
+            return min;
+        };
+        int min = findSmallestNumber.apply(numbers);
+        System.out.println(min);
+    }
+}
---
+++ Java OOP Basics/Functional Programming/Homework - Funtional Programming/Knights Of Honor/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Consumer<String> printList = name -> System.out.printf("Sir %s %n",name);
+
+
+        String[] input = scanner.nextLine().split("\\s+");
+
+        for (String name : input) {
+            printList.accept(name);
+        }
+    }
+}
